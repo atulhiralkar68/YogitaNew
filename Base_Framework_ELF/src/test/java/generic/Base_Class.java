59b5c7628958c22200b6402800033cac4fd136d1
atulhiralkar68 <atulhiralkar68@gmail.com>
2024-06-22 11:59:50 +0530

    Driver setup change

--- Base_Framework_ELF/src/test/java/generic/Base_Class.java
+++ Base_Framework_ELF/src/test/java/generic/Base_Class.java
@@ -5,14 +5,11 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * 
@@ -31,7 +28,7 @@ public class Base_Class implements FrameworkConstants{
 
 
 		if(browserName.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();,
+			//WebDriverManager.chromedriver().setup();
 			driver=new ChromeDriver();
 			/*
 			 * System.setProperty(CHROME_KEY,CHROME_PATH); driver=new ChromeDriver();
@@ -39,7 +36,7 @@ public class Base_Class implements FrameworkConstants{
 			 */
 		}else if(browserName.equalsIgnoreCase("firefox")) {
 
-			WebDriverManager.firefoxdriver().setup();
+			//WebDriverManager.firefoxdriver().setup();
 			driver=new FirefoxDriver();
 			/*
 				
@@ -53,7 +50,7 @@ public class Base_Class implements FrameworkConstants{
 		driver.manage().window().maximize();
 		Reporter.log("Browser window is maximized successfully",true);
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-		WebDriverWait explicitWait = new WebDriverWait(driver, 10);
+		// WebDriverWait explicitWait = new WebDriverWait(driver, SECONDS(10));
 		PropertyFileReader fileReader=new PropertyFileReader();
         String URL=fileReader.getValueProperty("URL");
 		driver.get(URL);
